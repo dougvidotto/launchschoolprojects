fcbe4811634266f07c796e3d644d94f21f566db6
Douglas Vidotto <doug.vidotto@gmail.com>
2017-01-05 01:24:23 -0200

    Correcting some rubocop offenses, adding validations to final answer and inserting a round number to mortgage value

--- lesson_2_small_programs/mortgage_calculator.rb
+++ lesson_2_small_programs/mortgage_calculator.rb
@@ -1,9 +1,8 @@
-
 def prompt(message)
   puts "=> #{message}"
 end
 
-def name_valid?(name)
+def valid_name?(name)
   if name.empty?
     false
   else
@@ -11,7 +10,7 @@ def name_valid?(name)
   end
 end
 
-def number_valid?(loan)
+def valid_number?(loan)
   if loan.to_f.to_s == loan && loan.to_f > 0
     true
   elsif loan.to_i.to_s == loan && loan.to_i > 0
@@ -29,16 +28,25 @@ def valid_duration?(duration)
   end
 end
 
+def valid_answer?(answer)
+  if answer.casecmp("y").zero? ||
+     answer.casecmp("n").zero?
+    true
+  else
+    false
+  end
+end
+
 prompt("Welcome to mortgage calculator!")
 prompt("Please, enter your name: ")
 name = gets.chomp.strip
 
 loop do
   loop do
-    if name_valid?(name)
+    if valid_name?(name)
       break
     else
-      prompt("You didnt insert a valid name. Please try again.")
+      prompt("Invalid name. Please try again.")
       name = gets.chomp.strip
     end
   end
@@ -47,22 +55,22 @@ loop do
   loan = gets.chomp.strip
 
   loop do
-    if number_valid?(loan)
+    if valid_number?(loan)
       break
     else
-      prompt("You didn't insert a valid value for loan. Please try again")
+      prompt("Invalid value for loan. Please try again")
       loan = gets.chomp.strip
     end
   end
 
-  prompt("#{name}, please insert the Annual Percentage Rate amount (only numbers) :")
+  prompt("#{name}, please insert the APR amount (only numbers) :")
   annual_percentage_rate = gets.chomp.strip
 
   loop do
-    if number_valid?(annual_percentage_rate)
+    if valid_number?(annual_percentage_rate)
       break
     else
-      prompt("You didn't insert a valid value for annual_percentage_rate. Please try again")
+      prompt("Invalid value for annual_percentage_rate. Please try again")
       annual_percentage_rate = gets.chomp.strip
     end
   end
@@ -74,21 +82,31 @@ loop do
     if valid_duration?(loan_duration_in_months)
       break
     else
-      prompt("You didn't insert a valid value for loan duration. Please try again")
+      prompt("Invalid value for loan duration. Please try again")
       loan_duration_in_months = gets.chomp.strip
     end
   end
 
   monthy_interest_rate = (annual_percentage_rate.to_f / 100) / 12
-  mortage = loan.to_f * (monthy_interest_rate / (1 - (1 + monthy_interest_rate)**-loan_duration_in_months.to_i))
+  mortage = loan.to_f * (monthy_interest_rate /
+            (1 - (1 + monthy_interest_rate)**-loan_duration_in_months.to_i))
 
-  prompt("The mortage rate will be: U$ #{mortage} per month")
+  prompt("The mortage rate will be: U$ #{mortage.round(2)} per month")
 
-  prompt("#{name}, would you like to calculate another mortgage? Type (y) for yes or anything else for no")
-
+  prompt("#{name}, calcula mortgage again? Type (y) for yes or (n) for no")
   answer = gets.chomp
 
-  break if answer.casecmp("y").zero?
+  loop do
+    if valid_answer?(answer)
+      break
+    else
+      prompt("Invalid answer. Type 'y' or 'n'.")
+      prompt("#{name}, calcula mortgage again? Type (y) for yes or (n) for no")
+      answer = gets.chomp
+    end
+  end
+
+  break if answer.casecmp("n").zero?
 end
 
 prompt("Thanks for using Mortage Calculator! Bye!")
