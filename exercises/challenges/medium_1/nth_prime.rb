638f06e067db0e4a17f93cd28515cefe1aff0d8a
Douglas Vidotto Carvalho <doug.vidotto@gmail.com>
2017-09-20 22:51:18 -0300

    Refactoring nth prime

--- exercises/challenges/medium_1/nth_prime.rb
+++ exercises/challenges/medium_1/nth_prime.rb
@@ -1,14 +1,20 @@
 class Prime
-  def self.nth(position)
-    raise ArgumentError if position.to_i < 1
-    prime_numbers = []
-    possible_prime = 2
-    while prime_numbers.size < position
-      possible_primes = prime_numbers.take_while {|num| num <= Math.sqrt(possible_prime).to_i }
-      has_factors = possible_primes.select { |prime_num| possible_prime % prime_num == 0 }
-      prime_numbers << possible_prime if has_factors.empty?
-      possible_prime += 1
+  def self.nth(quantity)
+    raise ArgumentError if quantity.to_i < 1
+    prime_numbers = [2]
+    possible_prime = 3
+    while prime_numbers.size < quantity
+      
+      primes_shorter_than_possible_prime = prime_numbers.take_while do 
+        |prime_num| prime_num <= Math.sqrt(possible_prime).to_i
+      end
+      
+      prime_numbers << possible_prime unless primes_shorter_than_possible_prime.any? do 
+        |prime_num| possible_prime % prime_num == 0
+      end
+      
+      possible_prime += 2
     end
-    prime_numbers[position - 1]
+    prime_numbers[quantity - 1]
   end
 end
