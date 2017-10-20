6754e0b15df1bd0c79289aaed253eca522aafb6f
Douglas Vidotto Carvalho <doug.vidotto@gmail.com>
2017-10-20 21:06:00 -0200

    A fancier solution for crypto square

--- exercises/challenges/medium_2/crypto_square.rb
+++ exercises/challenges/medium_2/crypto_square.rb
@@ -1,12 +1,7 @@
-# Algorithm
-# Dividing into squares
-# pick up text and iterate through its characters
-# when index reaches the size position we add those characters until that position into an array
-
 class Crypto
   attr_reader :size, :plaintext_segments
   def initialize(text)
-    @text = text.gsub(/\W/, "").downcase
+    @text = text.gsub(/[^a-zA-Z0-9]/, "").downcase
     @size = Math.sqrt(@text.size).ceil
     @plaintext_segments = create_plaintext_segments
   end
@@ -20,33 +15,17 @@ class Crypto
   end
 
   def normalize_ciphertext
-    cipher_position = 0
-    cipher_segments = []
-    loop do
-      break if cipher_position == size
-      position = 0
-      cipher_segment = ""
-      loop do
-        break if position == plaintext_segments.size
-        cipher_segment += plaintext_segments[position][cipher_position].to_s
-        position += 1
-      end
-      cipher_segments << cipher_segment
-      cipher_position += 1
-    end
-    cipher_segments.join(" ")
+    plaintext_segments.each_with_object(Array.new(size){|_| ""}) do |segment, cipher|
+      segment.chars.map.with_index {|char, idx| cipher[idx] += char}
+    end.join(" ")
   end
 
   private
 
   def create_plaintext_segments
-    position = 0
-    segments = []
-    loop do
-      break if position >= @text.size
-      segments << @text[position...position + size]
-      position += size
-    end
-    segments
+    @text.chars.each_slice(size).map {|slice| slice.join}
   end
 end
+
+crypto = Crypto.new('Vampires are people too!')
+p crypto.normalize_ciphertext
